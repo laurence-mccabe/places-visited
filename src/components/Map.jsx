ae8c68c0e68ef057549ee610322bb813174ef476
laurence0202 <laurence0202@users.noreply.github.com>
2023-08-09 11:50:18 +1000

    added changeCenter and useEffect to update map position dynamically in Map.jsx leaflet

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,24 +1,36 @@
 import { useNavigate, useSearchParams } from 'react-router-dom'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 
 import styles from './Map.module.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useCities } from '../contexts/CitiesContext'
+import { map } from 'leaflet'
 
 const Map = () => {
   const navigate = useNavigate()
   const { cities } = useCities()
-  const [mapPosition, setMapPosition] = useState([51.505, -0.09])
 
+  const [mapPosition, setMapPosition] = useState([51.5072, 0.1276])
   const [searchParams, setSearchParams] = useSearchParams()
-  const lat = searchParams.get('lat')
-  const lng = searchParams.get('lng')
+
+  const mapLat = searchParams.get('lat');
+  const mapLng = searchParams.get('lng');
+
+  useEffect(() => {
+    console.log('mapLat and mapLng', mapLat, mapLng)
+    if (mapLat && mapLng)
+    {
+    setMapPosition([mapLat, mapLng])}
+  }, [mapLat, mapLng])
+
 
   return (
     <div className={styles.mapContainer}>
       <MapContainer
+        // center={mapLat && mapLng ? [mapLat, mapLng] : mapPosition}
+        // center={[mapLat, mapLng]}
         center={mapPosition}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
       >
@@ -32,15 +44,24 @@ const Map = () => {
             key={city.id}
           >
             <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
+              <span>{city.emoji}<h5>{city.cityName}<br></br> {city.notes}</h5> </span>
             </Popup>
+            {/* <ChangeCenter position={[city.position.lat, city.position.lng]} /> */}
           </Marker>
         ))}
+        
+        <ChangeCenter position={mapPosition} />
       </MapContainer>
       {/* <h1>Postion: {lat}, {lng}</h1>
       <button onClick={() => {setSearchParams({ lat:23, lng:50 })}}>Change positon</button> */}
     </div>
   )
+}
+
+const ChangeCenter = ({ position }) => {
+  const map = useMap()
+  map.setView(position)
+  return null;
 }
 
 export default Map
