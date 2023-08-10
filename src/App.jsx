8cb52541fb0db5e5dd4a4d0a758a00446e2a68d1
laurence0202 <laurence0202@users.noreply.github.com>
2023-08-10 18:57:56 +1000

    temp commit before creating wendel pr branch

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,11 @@ import CountryList from './components/CountryList'
 import City from './components/City'
 import Form from './components/Form'
 import { CitiesProvider } from './contexts/CitiesContext'
+import { AuthProvider } from './contexts/FakeAuthContext'
 
 const App = () => {
   return (
+    <AuthProvider>
     <CitiesProvider>
       <BrowserRouter>
         <Routes>
@@ -32,8 +34,18 @@ const App = () => {
         </Routes>
       </BrowserRouter>
     </CitiesProvider>
+    </AuthProvider>
   )
 }
+/*
+CHALLENGE
+
+1) Add `AuthProvider` to `App.jsx`
+2) In the `Login.jsx` page, call `login()` from context
+3) Inside an effect, check whether `isAuthenticated === true`. If so, programatically navigate to `/app`
+4) In `User.js`, read and display logged in user from context (`user` object). Then include this component in `AppLayout.js`
+5) Handle logout button by calling `logout()` and navigating back to `/`
+*/
 
 export default App
 
